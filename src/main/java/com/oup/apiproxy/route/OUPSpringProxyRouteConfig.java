4240575c96c1106badfd3ad06985226c47dfdc41
Saibal Kumar Ghosh <Saibal.KumarGhosh.Contractor@oup.com>
2019-04-09 19:46:17 +0100

    NA

--- src/main/java/com/oup/apiproxy/route/OUPSpringProxyRouteConfig.java
+++ src/main/java/com/oup/apiproxy/route/OUPSpringProxyRouteConfig.java
@@ -1,5 +1,7 @@
 package com.oup.apiproxy.route;
 
+import java.io.IOException;
+
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,35 +12,57 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder.Builder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+import io.kubernetes.client.ApiClient;
+import io.kubernetes.client.ApiException;
+import io.kubernetes.client.apis.CoreV1Api;
+import io.kubernetes.client.util.Config;
 
 @Configuration
 @RefreshScope
 public class OUPSpringProxyRouteConfig {
 	Logger logger = org.slf4j.LoggerFactory.getLogger(this.getClass());
+	
 	@Autowired
-	private DiscoveryClient discoveryClient;
+	private Environment environment;
 
 	@Value("${spring.application.name}")
 	private String appName;
 
 	@RefreshScope
 	@Bean
-	public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+	public RouteLocator routeLocator(RouteLocatorBuilder builder) throws IOException, ApiException {
 		Builder bldr = builder.routes();
+		ApiClient client = Config.defaultClient();
+		io.kubernetes.client.Configuration.setDefaultApiClient(client);
 
-		discoveryClient.getServices().forEach(item -> {
-			String applicationName = item;
-
-			if (!applicationName.equalsIgnoreCase(appName))
+		CoreV1Api api = new CoreV1Api();
+		api.listNamespacedService(environment.getActiveProfiles()[0], null, null, null, null, null, null, null, null,
+				null).getItems().forEach(item -> {
+					String applicationName = item.getMetadata().getName();
+					if (!applicationName.equalsIgnoreCase(appName))
 
 			{
-				logger.info("Binding service: " + applicationName);
+						logger.info("Binding service: " + applicationName);
 
-				bldr.route(r -> r.path("/" + applicationName + "/**")
-						.filters(f -> f.rewritePath("/" + applicationName + "/(?<path>.*)", "/$\\{path}"))
-						.uri("http://" + applicationName + "").id(applicationName));
-			}
-		});
+						bldr.route(r -> r.path("/" + applicationName + "/**")
+								.filters(f -> f.rewritePath("/" + applicationName + "/(?<path>.*)", "/$\\{path}"))
+								.uri("http://" + applicationName + "").id(applicationName));
+					}
+				});
+		/*
+		 * discoveryClient.getServices().forEach(item -> { String applicationName =
+		 * item;
+		 * 
+		 * if (!applicationName.equalsIgnoreCase(appName))
+		 * 
+		 * { logger.info("Binding service: " + applicationName);
+		 * 
+		 * bldr.route(r -> r.path("/" + applicationName + "/**") .filters(f ->
+		 * f.rewritePath("/" + applicationName + "/(?<path>.*)", "/$\\{path}"))
+		 * .uri("http://" + applicationName + "").id(applicationName)); } });
+		 */
 
 		// bldr.route(r -> r.path("/uam**")
 		// .filters(f -> f.rewritePath("/uam/(?<path>.*)", "/$\\{path}"))
