4b84612245f204d4e04cd006f583d1325be92288
Saibal Kumar Ghosh <Saibal.KumarGhosh.Contractor@oup.com>
2019-07-23 15:38:51 +0100

    NA

--- src/main/java/com/oup/apiproxy/OupSpringApiGatewayApplication.java
+++ src/main/java/com/oup/apiproxy/OupSpringApiGatewayApplication.java
@@ -3,12 +3,15 @@ package com.oup.apiproxy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@ComponentScan("com.oup")
 public class OupSpringApiGatewayApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(OupSpringApiGatewayApplication.class, args);
 	}
 }
